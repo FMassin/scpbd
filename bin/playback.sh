598ebcec8f2b9178165fdc2d69a97fc6fe460c03
Fred Massin <FMassin@users.noreply.github.com>
2024-09-23 14:24:53 +0200

    Safety measure

--- bin/playback.sh
+++ bin/playback.sh
@@ -9,8 +9,8 @@ ls $DB  || \
      sqlite3 -batch -init $SEISCOMP_ROOT/share/db/vs/sqlite3.sql $DB .exit && \
      sqlite3 -batch -init $SEISCOMP_ROOT/share/db/wfparam/sqlite3.sql $DB .exit)
 
-import_inv help formats |grep $3
-import_inv $3 $2          
+import_inv help formats |grep $3 || exit 1
+import_inv $3 $2 || exit 1         
 
 FF=$SEISCOMP_ROOT/var/run/seedlink/mseedfifo 
 (echo "msrtsimul = true";echo "plugins.mseedfifo.fifo = $FF") > $SEISCOMP_ROOT/etc/global.cfg 
@@ -20,18 +20,19 @@ echo "sources = g:mseedfifo" > $D/profile_g
 
 python3 /usr/local/bin/mseed2key.py \
     $1 $SEISCOMP_ROOT/ \
-    "seedlink:g"
-seiscomp  update-config    
+    "seedlink:g" || exit 1
+seiscomp  update-config    || exit 1
 
 (ls $FF 2>/dev/null || ( mkdir -p $(dirname $FF) && mkfifo $FF )) 
 
 scmssort -u -E -v $1 > /tmp/sorted.mseed 
 
-seiscomp enable seedlink
+seiscomp enable seedlink || exit 1
 
 #(seiscomp restart seedlink && msrtsimul "${@:4}" /tmp/sorted.mseed) 
 
-cp $4 /home/sysop/event_db.sqlite
+cp $4 /home/sysop/event_db.sqlite 
+
 seiscomp exec python3 \
     /home/sysop/sc3-playback/playback.py \
     /home/sysop/event_db.sqlite \
